5824b5910c3a957f11d8957b4edb46bae7b3ae5c
shiroichi28 <manigandans28@gmail.com>
2023-06-15 22:27:10 +0530

    Intial Commit

---
+++ src/api.jsx
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7d1cbe7127mshabcc8160520f527p13cd10jsn0da1cd035075',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="2b26f4b582d845a55a61e30e38a21a9e";
